a5d51cea0cc81b1fdcb9f80bc821461281dffda8
Vianney le Clément de Saint-Marcq <vianney.leclement@pythia-project.org>
2013-10-07 08:08:11 +0200

    Remove busybox

--- tools/mkrootfs/pythia.mk
+++ tools/mkrootfs/pythia.mk
@@ -22,62 +22,13 @@ $(MKROOTFS_INIT): $(MKROOTFS_INIT_SOURCES)
 	$(CC) -static -m32 -O3 -o $@ $*
 	strip $@
 
-BUSYBOX_CONFIG := $~/busybox.config
-BUSYBOX_VERSION := 1.21.0
-BUSYBOX_OUTPUT := $(MKROOTFS_BUILD_DIR)/busybox
-
-BUSYBOX_DIR := $(MKROOTFS_BUILD_DIR)/busybox-$(BUSYBOX_VERSION)
-BUSYBOX_TREE := $(BUSYBOX_DIR)/extracted.stamp
-BUSYBOX_ARCHIVE := $(BUSYBOX_DIR).tar.bz2
-BUSYBOX_URL := http://busybox.net/downloads/$(notdir $(BUSYBOX_ARCHIVE))
-
-BUSYBOX_MAKE := $(MAKE) -C $(BUSYBOX_DIR) CC="$(CC) -m32" HOSTCC="$(CC) -m32"
-
 MKROOTFS_DEPENDS := \
 	$~/mkrootfs.sh \
-	$~/busybox.links \
 	$~/functions.sh \
-	$(MKROOTFS_INIT) \
-	$(BUSYBOX_OUTPUT) \
+	$(MKROOTFS_INIT)
 MKROOTFS := $~/mkrootfs.sh -b $(MKROOTFS_BUILD_DIR)
 
-$(call add_target, busybox, BUILD, Build busybox)
-busybox: $(BUSYBOX_OUTPUT)
-
-$(BUSYBOX_OUTPUT): $(BUSYBOX_CONFIG) $(BUSYBOX_TREE)
-	cp $(BUSYBOX_CONFIG) $(BUSYBOX_DIR)/.config
-	$(BUSYBOX_MAKE)
-	cp $(BUSYBOX_DIR)/busybox $@
-
-# The busybox build directory is secondary, so we can manually remove it after
-# the compilation completed and it will not trigger a new extraction and
-# compilation.
-.SECONDARY: $(BUSYBOX_TREE)
-$(BUSYBOX_TREE): $(BUSYBOX_ARCHIVE)
-	@mkdir -p $(BUSYBOX_DIR)
-	tar -xj -C $(BUSYBOX_DIR) -f $(BUSYBOX_ARCHIVE) --strip-components=1
-	touch $@
-
-$(BUSYBOX_ARCHIVE):
-	@mkdir -p $(@D)
-	wget -O $@ $(BUSYBOX_URL)
-
-$(call add_target, busybox_menuconfig, MISC, Configure busybox)
-busybox_menuconfig: $(BUSYBOX_TREE)
-	[ -e $(BUSYBOX_CONFIG) ] \
-		&& cp -p $(BUSYBOX_CONFIG) $(BUSYBOX_DIR)/.config \
-		|| $(BUSYBOX_MAKE) defconfig
-	$(BUSYBOX_MAKE) menuconfig
-	cp -p $(BUSYBOX_DIR)/.config $(BUSYBOX_CONFIG)
-
-$(call add_target, busybox_oldconfig, MISC, Upgrade busybox configuration)
-busybox_oldconfig: $(BUSYBOX_TREE)
-	cp -p $(BUSYBOX_CONFIG) $(BUSYBOX_DIR)/.config
-	$(BUSYBOX_MAKE) oldconfig
-	cp -p $(BUSYBOX_DIR)/.config $(BUSYBOX_CONFIG)
-
 clean::
-	-rm -rf $(BUSYBOX_DIR)
-	-rm $(BUSYBOX_OUTPUT)
+	-rm $(MKROOTFS_INIT)
 
 # vim:set ts=4 sw=4 noet:
