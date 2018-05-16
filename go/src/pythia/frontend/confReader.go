f4f5ccc2adbecbdb425c028de9e845fd28e8e066
nathan <nathan@hashcode.com>
2018-05-16 15:55:01 +0200

    Loading only once the config.json file

--- go/src/pythia/frontend/confReader.go
+++ go/src/pythia/frontend/confReader.go
@@ -2,19 +2,34 @@ package frontend
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-//Conf struct for the conf.json file
+//Conf struct for the config.json file
 type Conf struct {
 	IP []string
 }
 
-//GetConf to retreive the data from the conf.json file
+var myConf Conf
+
+//GetConf that was or will be loaded from the config.json file
 func GetConf() Conf {
+	//check if not initialized
+	if cap(myConf.IP) == 0 {
+		fmt.Println("ip = 0")
+		myConf = LoadConf()
+	}
+	fmt.Println("sending conf")
+	return myConf
+}
+
+//LoadConf to retreive the data from the conf.json file
+func LoadConf() Conf {
 	var conf Conf
-	c, err := ioutil.ReadFile("conf.json")
+	//Problem with the congif.json file: it must be in the go/bin/ directory to be loaded
+	c, err := ioutil.ReadFile("./config.json")
 	if err != nil {
 		log.Println("Could not open conf file, default configuration loaded")
 		return LoadDefaultConf()
@@ -29,7 +44,6 @@ func GetConf() Conf {
 }
 
 func LoadDefaultConf() Conf {
-	conf := Conf{[]string{"127.0.0.1"}}
-	//append(Conf.IP, "127.0.0.1")
+	conf := Conf{[]string{"127.0.0.1", "::1"}}
 	return conf
 }
